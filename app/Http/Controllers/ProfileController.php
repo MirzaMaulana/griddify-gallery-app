d40045b80cc218dfca9252256e601e7c6338fa2c
MirzaMaulana <mirzaartstudio35@gmail.com>
2024-04-14 10:56:57 +0700

    feat: function profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Picture;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -14,4 +16,36 @@ class ProfileController extends Controller
         $myPictures = Picture::with('user')->where('user_id', $profile->id)->paginate(6);
         return inertia('profile/my-post', ['user' => $profile, 'myPictures' => $myPictures]);
     }
+
+    // public function update(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required|string|max:255',
+    //         'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // max 2MB
+    //     ]);
+
+    //     $user = Auth::user();
+
+    //     if (!$user) {
+    //         abort(403, 'Unauthorized action.');
+    //     }
+
+    //     $user->name = $request->name;
+
+    //     if ($request->hasFile('avatar')) {
+    //         // Delete existing avatar if it exists
+    //         if ($user->avatar) {
+    //             Storage::delete('public/avatars/' . $user->avatar);
+    //         }
+
+    //         $avatar = $request->file('avatar');
+    //         $avatarName = $user->id . '_' . time() . '.' . $avatar->getClientOriginalExtension();
+    //         $avatar->store('public/avatars');
+    //         $user->avatar = $avatarName;
+    //     }
+
+    //     $user->save();
+
+    //     return redirect()->route('profile.index')->with('success', 'Profile updated successfully.');
+    // }
 }
